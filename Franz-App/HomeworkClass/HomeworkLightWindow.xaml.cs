ea2fa282d15c5aa430d72dbc5d5b97d6f8ab6908
Tom Fangmeyer <tom01@fangmeyer.org>
2021-06-30 22:18:57 +0200

    stable Front

---
+++ Franz-App/HomeworkClass/HomeworkLightWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Franz_App.HomeworkClass
+{
+    /// <summary>
+    /// Interaktionslogik für HomeworkLightWindow.xaml
+    /// </summary>
+    public partial class HomeworkLightWindow : Page
+    {
+        public HomeworkLightWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Franz-App/MainClass/MainLightWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Franz_App.MainClass
+{
+    /// <summary>
+    /// Interaktionslogik für MainLightWindow.xaml
+    /// </summary>
+    public partial class MainLightWindow : Page
+    {
+        public MainLightWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
